ac9e2416846c1c112f35fa58be795bf1ca926a85
Áng_Ssyy <115669047+angel1231245@users.noreply.github.com>
2025-03-08 09:31:56 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,8 +100,8 @@
                 <div
                     class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
                     <!-- Imagen con margen superior -->
-                    <img src="img/meca.png" alt="Gráfica Dinámica"
-                         class="w-full h-full object-cover mt-8">
+                    <img src="img/loh.png" alt="Gráfica Dinámica"
+                 class="w-full h-full object-cover mt-8">
                     <div
                         class="absolute inset-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"></div>
                 </div>
